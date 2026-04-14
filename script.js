7ea609c2fb65b0a7baad830b934f9318a58e239a
uapm <93893594+uapm@users.noreply.github.com>
2026-04-14 12:06:03 +0200

    Create Draft of function to make a div grid

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // script will be here
+const container = document.querySelector('.container');
+const pixel = document.createElement('div');
+let res = 16;
+
+
+function makeGrid(resolution) {
+	const pixDim = `${10 / resolution}cm`
+	pixel.style.width = pixDim;
+	pixel.style.height = pixDim;
+	pixel.style.border = 'solid, black';
+	container.appendChild(pixel);
+	for (let i = 1; i < resolution ** 2 && i < 10000; i++) {
+	const looPix = pixel.cloneNode(true);
+	container.appendChild(looPix);
+	}
+}	
+
+
+
+
+
+
+
+
+
+
+
