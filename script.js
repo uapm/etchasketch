0c4e8b9b248f916121f3914e64fde734d03abf0d
uapm <93893594+uapm@users.noreply.github.com>
2026-04-15 11:10:25 +0200

    Add function which changes color of one div on mouseover

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-// script will be here
 const container = document.querySelector('.container');
 const pixel = document.createElement('div');
 let res = 16;
 
 
+
 function makeGrid(resolution) {
-	const pixDim = `${10 / resolution}cm`
+	let looPix;
+	function changeColor() {
+		if (opacity !== 100) {
+			opacity += 10;
+			dynColor = `rgb(0 0 0 / ${opacity}%)`;
+			pixel.style.background = dynColor;
+			console.log(dynColor)
+		}
+	}
+	const pixDim = `${15 / resolution}cm`
 	pixel.style.width = pixDim;
 	pixel.style.height = pixDim;
-	pixel.style.border = 'solid, black';
+	pixel.style.border = '0.1px solid, #00000020';
+	let opacity = 0;
+	let dynColor = `rgb(0 0 0 / ${opacity}%)`;
+	pixel.style.background = dynColor;
+	pixel.addEventListener('mouseover', changeColor);
 	container.appendChild(pixel);
 	for (let i = 1; i < resolution ** 2 && i < 10000; i++) {
-	const looPix = pixel.cloneNode(true);
+	looPix = pixel.cloneNode(true);
 	container.appendChild(looPix);
 	}
-}	
-
-
-
-
-
-
-
-
-
-
-
+}
